d773146f0b3da09bf09683a0f8118759a7750963
Gui Hebling <guilherme.durso@outlook.com>
2020-11-12 00:16:19 +0100

    New Loan Calculator

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ let loanGoal = document.getElementById("loan-goal");
 let loanAmount = document.getElementById("loan-amount");
 let loanPeriod = document.getElementById("loan-period");
 
+Number.prototype.toLocaleNorway = function () {
+    return this.toLocaleString("no-NO", {
+        style: "currency", currency: "NOK"
+    });
+};
+
 function PMT(rate, nper, pv, fv, type) {
     /*
     PMT formula is based on an Excel function
@@ -31,12 +37,17 @@ function PMT(rate, nper, pv, fv, type) {
         pmt /= (1 + rate);
     };
 
-    return Math.abs(pmt).toFixed(2);
+    return Math.abs(pmt);
 }
 
 function serialLoan(rate, period, pv, instYr) {
     /* 
-    instYr:    instalments per year, 12 if omitted
+    rate:       interest rate per period
+    period:     period of the loan (in years)
+    pv:         present value or the loan amount
+    instYr:     instalments per year, 12 if omitted
+
+    numInst:    number of instalments
       */
 
     if (!instYr) instYr = 12;
@@ -49,12 +60,6 @@ function serialLoan(rate, period, pv, instYr) {
     payment = repayment + interest;
     principal = (pv - repayment);
 
-    return {
-        interest,
-        repayment,
-        payment,
-        principal
-    };
 }
 
 function setInterestRate() {
@@ -87,6 +92,7 @@ function logPMT() {
     let loanPeriod = document.getElementById("loan-period").value;
     const loanType = document.getElementById("loan-type").value;
     const summaryPayment = document.getElementById("summary-payment");
+    const tableHeader = document.getElementById("table-header");
     const tableBody = document.getElementById("table-body");
 
     setInterestRate();
@@ -109,67 +115,119 @@ function logPMT() {
         summaryPayment.innerHTML = `
         <h3>Your Summary</h3>
         `;
+        tableHeader.innerHTML = "";
+        tableBody.innerHTML = "";
 
 
         if (loanType === "serial-loan-type") {
-            summaryPayment.innerHTML = `
-            <p> Your first monthly payment is <span> ${payment.toFixed(2)} kr </span></p>
-            <h4>Report:</h4>
-            `;
-            tableBody.innerHTML = `
-                <table>
-                <thead>
-                <tr>
-                    <th>Period</th>
-                    <th>First Payment</th>
-                    <th>First Interest Payment</th>
-                    <th>Fixed Repayment</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>${numInst.toFixed(0)}</td>   
-                    <td>${payment.toFixed(2)} kr</td>
-                    <td>${interest.toFixed(2)} kr</td>
-                    <td>${repayment.toFixed(2)} kr</td>
-                </tr>
-                </tbody>
-                </table>
-                `
+
+            // Calculating Next Instalments for Serial Loan
+            nextInstalments(
+                interest,
+                numInst,
+                payment,
+                repayment
+            );
+
+            function nextInstalments(interest, numInst, payment, repayment) {
+                let instalmentArray = [];
+                let interestArray = [];
+                let principalArray = [];
+                let repaymentArray = [];
+                summaryPayment.innerHTML = `
+                <p> Your first monthly payment is <span> ${payment.toLocaleNorway()} </span></p>
+                <h4>Report:</h4>
+                `;
+                tableHeader.innerHTML = `
+                    <tr>
+                        <th>Period</th>
+                        <th>Instalment</th>
+                        <th>Interest</th>
+                        <th>Repayment</th>
+                        <th>Remain</th>
+                    </tr>
+                    `
+                for (let i = 0; i < numInst; i++) {
+                    if (i === 0) {
+                        principal = principal;
+                        interest = interest;
+                        repayment = repayment;
+                        payment = payment;
+
+                        instalmentArray.push(payment);
+                        interestArray.push(interest);
+                        principalArray.push(principal);
+                        repaymentArray.push(repayment);
+
+                        tableBody.innerHTML += `
+                        <tr>
+                            <td>${i + 1}</td>   
+                            <td>${payment.toLocaleNorway()}</td>
+                            <td>${interest.toLocaleNorway()}</td>
+                            <td>${repayment.toLocaleNorway()}</td>
+                            <td>${principal.toLocaleNorway()}</td>
+                        </tr>
+                    `
+
+                    } else {
+                        interest = principal * ratePerInst;
+                        repayment = repayment;
+                        payment = repayment + interest;
+                        principal = principal - repayment;
+
+                        instalmentArray.push(payment);
+                        interestArray.push(interest);
+                        principalArray.push(principal);
+                        repaymentArray.push(repayment);
+
+                        tableBody.innerHTML += `
+                            <tr>
+                                <td>${i + 1}</td>   
+                                <td>${payment.toLocaleNorway()}</td>
+                                <td>${interest.toLocaleNorway()}</td>
+                                <td>${repayment.toLocaleNorway()}</td>
+                                <td>${principal.toLocaleNorway()}</td>
+                            </tr>
+                        `
+
+                    }
+                }
+            }
+
+
         } else if (loanType === "annuity-loan-type") {
             summaryPayment.innerHTML = `
-            <p> The monthly payment is <span> ${resultPMT} kr </span></p>
+            <p> The monthly payment is <span> ${resultPMT.toLocaleNorway()}</span></p>
             <h4>Report:</h4>
             `;
 
+            tableHeader.innerHTML = `
+            <tr>
+                <th>Period</th>
+                <th>Fixed Monthly Payment</th>
+                <th>Total Cost</th>
+                <th>Total Interest Paid</th>
+            </tr>
+            `
+
             tableBody.innerHTML = `
-                <table>
-                <thead>
-                <tr>
-                    <th>Period</th>
-                    <th>Monthly Payment</th>
-                    <th>Total Cost</th>
-                    <th>Interest Paid</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>${loanPeriod} years</td>   
-                    <td>${resultPMT} kr</td>
-                    <td>${(resultPMT * (loanPeriod * 12)).toLocaleString("no-NO")} kr</td>
-                    <td>${((resultPMT * (loanPeriod * 12)) - loanAmount).toLocaleString("no-NO")} kr</td>
-                </tr>
-                </tbody>
-                </table>
-                `
+            <tr>
+                <td>${loanPeriod} years</td>   
+                <td>${resultPMT.toLocaleNorway()}</td>
+                <td>${(resultPMT * (loanPeriod * 12)).toLocaleNorway()}</td>
+                <td>${((resultPMT * (loanPeriod * 12)) - loanAmount).toLocaleNorway()}</td>
+            </tr>
+            `
         }
     }
 
     if (!loanAmount || !interestRate || !loanPeriod) {
         summaryPayment.innerHTML = "Please fill in all the required fields.";
+        tableHeader.innerHTML = "";
         tableBody.innerHTML = "";
     } else if (loanAmount === "0" || interestRate === "0" || loanPeriod === "0") {
         summaryPayment.innerHTML = "The values must be above zero.";
+        tableHeader.innerHTML = "";
         tableBody.innerHTML = "";
     } else {
         printResults();
